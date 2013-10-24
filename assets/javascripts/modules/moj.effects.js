e21f59d5dc79bdcd41bbae0e541fd8accd48bbbd
Clive Murray <clive.murray@digital.justice.gov.uk>
2013-10-24 16:15:29 +0100

    Added effects module with highlight effect

---
+++ assets/javascripts/modules/moj.effects.js
@@ -0,0 +1,74 @@
+/*jslint browser: true, evil: false, plusplus: true, white: true, indent: 2 */
+/*global moj, $ */
+
+moj.Modules.effects = (function() {
+  "use strict";
+
+  var init,
+      highlight,
+      highlights;
+
+  /*
+    DEPENDENCIES
+    ============
+    jQuery, moj, some method of animating colours
+
+    jQuery cannot animate colours natively so unless you already have jQuery UI installed, a plugin is needed.
+    Try one of:
+    - http://api.jqueryui.com/category/effects-core/
+    - https://github.com/jquery/jquery-color/
+    - http://www.bitstorm.org/jquery/color-animation/
+
+
+    USAGE
+    =====
+    
+    trigger fade effect on single element with default colour and duration: 
+    moj.Modules.effects.highlight($('#alert'));
+
+    trigger fade effect on single element with custom colour and duration:
+    moj.Modules.effects.highlight($('#alert'), {colour: '#0f0', duration: 5000});
+
+    trigger fade effect on multiple elements with common className:
+    moj.Modules.effects.highlights({class: 'newRow'});
+
+    trigger fade effect on multiple elements with common className and custom colour and/or duration:
+    moj.Modules.effects.highlights({class: 'newRow', colour: '#0f0', duration: 5000});
+  */
+
+  init = function() {
+    // do nothing
+    // must be present for moj module loader
+    // unfortunately causes JSLint to error, but hey, what you gonna do?
+  };
+
+  highlights = function( options ) {
+    var elclass = ( options && options.class ? options.class : 'highlight' ),
+        els = $( '.' + elclass );
+    
+    $( els ).each( function() {
+      highlight( $( this ), options );
+    } );
+  };
+
+  highlight = function( $el, options ) {
+    var elclass = ( options && options.class ? options.class : 'highlight' ),
+        col = ( options && options.colour ? options.colour : '#ff9' ),
+        duration = ( options && options.duration ? options.duration : 1500 ),
+        obg = $el.css( 'background-color' );
+
+    $el.css( 'background-color', col ).animate( {
+      backgroundColor:  obg
+    }, duration, function() {
+      $el.removeClass( elclass );
+    } );
+  };
+
+  // public
+
+  return {
+    init: init,
+    highlight: highlight,
+    highlights: highlights
+  };
+} () );
